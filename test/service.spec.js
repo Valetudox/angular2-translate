fb7541541d6307a844bdcb62b5e0af023a1f2682
Somodi Viktor <somodi.viktor@gmail.com>
2015-12-29 06:59:35 +0100

    test(service): add

--- test/service.spec.js
+++ test/service.spec.js
@@ -3,17 +3,45 @@
 import 'es6-shim';
 import 'reflect-metadata';
 const { expect } = require('chai');
-import { TranslateService } from '../lib/service';
+import { TranslateService } from '../lib';
 
 describe('Service', function() {
 
   describe('#translate', function() {
 
-    it('should translate the given simple text', function() {
-      let service = new TranslateService();
-      service.setTranslations({ key: 'value' });
+    let service;
 
+    beforeEach(function() {
+      service = new TranslateService();
+    });
+
+    it('should translate the given simple text', function() {
+      service.setTranslations({ key: 'value' });
       expect(service.translate('key')).to.eql('value');
+    });
+
+
+    it('should give back the given text if there is no translation for it', function() {
+      service.setTranslations({ key: 'value' });
+      expect(service.translate('wrongKey')).to.eql('wrongKey');
+    });
+
+
+    it('should give back the given text with interpolation', function() {
+      service.setTranslations({ key: '%s value %s' });
+      expect(service.translate('key', ['a', 'b'])).to.eql('a value b');
+    });
+
+
+    it('should handle nested dictionary', function() {
+      service.setTranslations({ root: { nested: 'value' } });
+      expect(service.translate('root.nested')).to.eql('value');
+    });
+
+
+    it('should give back the given text with interpolation if there is no translation for it', function() {
+      service.setTranslations({ key: 'value' });
+      expect(service.translate('%s wrongKey %s', ['a', 'b'])).to.eql('a wrongKey b');
     });
 
   });
