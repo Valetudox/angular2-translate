3e5be741cb383d8f02d175a0f304ca4f8718a3b7
Somodi Viktor <somodi.viktor@gmail.com>
2016-01-31 14:20:31 +0100

    test(module): use ava

--- test/pipe.spec.js
+++ test/pipe.spec.js
@@ -1,19 +1,19 @@
 import 'es6-shim';
 import 'reflect-metadata';
 import { TranslatePipe, TranslateService } from '../lib';
-import { expect } from 'chai';
+import { test } from 'ava';
 
-describe('Pipe', function() {
+test('#transform should delegate the translation to the service with empty arguments if there is no', function(t) {
+  const service = new TranslateService({ nested: { key: 'value%s' } });
+  const pipe = new TranslatePipe(service);
 
-  describe('#transform', function() {
-
-    it('should delegate the translation to the service', function() {
-      let service = new TranslateService({ nested: { key: 'value' } });
-      let pipe = new TranslatePipe(service);
-
-      expect(pipe.transform('nested.key')).to.eql('value');
-    });
-
-  });
-
+  t.is(pipe.transform('nested.key', [5]), 'value');
 });
+
+test('#transform should delegate the translation to the service', function(t) {
+  const service = new TranslateService({ nested: { key: 'value' } });
+  const pipe = new TranslatePipe(service);
+
+  t.is(pipe.transform('nested.key'), 'value');
+});
+
--- test/service.spec.js
+++ test/service.spec.js
@@ -1,63 +1,52 @@
 import 'es6-shim';
 import 'reflect-metadata';
-import { expect } from 'chai';
+import { test } from 'ava'
 import { TranslateService } from '../lib';
 
-describe('Service', function() {
+const createService = function(translations) {
+  return new TranslateService(translations);
+};
 
-  let createService = function(translations) {
-    return new TranslateService(translations);
-  };
-
-  describe('#translate', function() {
-
-    it('should translate the given simple text', function() {
-      let service = createService({ key: 'value' });
-      expect(service.translate('key')).to.eql('value');
-    });
+test('#translate should translate the given simple text', function(t) {
+  const service = createService({ key: 'value' });
+  t.is(service.translate('key'), 'value');
+});
 
 
-    it('should give back the given text if there is no translation for it', function() {
-      let service = createService({ key: 'value' });
-      expect(service.translate('wrongKey')).to.eql('wrongKey');
-    });
+test('#translate should give back the given text if there is no translation for it', function(t) {
+  const service = createService({ key: 'value' });
+  t.is(service.translate('wrongKey'), 'wrongKey');
+});
 
 
-    it('should give back the given text with interpolation', function() {
-      let service = createService({ key: '%s value %s' });
-      expect(service.translate('key', ['a', 'b'])).to.eql('a value b');
-    });
+test('#translate should give back the given text with interpolation', function(t) {
+  const service = createService({ key: '%s value %s' });
+  t.is(service.translate('key', ['a', 'b']), 'a value b');
+});
 
 
-    it('should handle nested dictionary', function() {
-      let service = createService({ root: { nested: 'value' } });
-      expect(service.translate('root.nested')).to.eql('value');
-    });
+test('#translate should handle nested dictionary', function(t) {
+  const service = createService({ root: { nested: 'value' } });
+  t.is(service.translate('root.nested'), 'value');
+});
 
 
-    it('should give back the given text with interpolation if there is no translation for it', function() {
-      let service = createService({ key: 'value' });
-      expect(service.translate('%s wrongKey %s', ['a', 'b'])).to.eql('a wrongKey b');
-    });
+test('#translate should give back the given text with interpolation if there is no translation for it', function(t) {
+  const service = createService({ key: 'value' });
+  t.is(service.translate('%s wrongKey %s', ['a', 'b']), 'a wrongKey b');
+});
 
 
-    it('should remove the interpolation places if they are not match for the expectations', function() {
-      let service = createService({ key: '%s value %d' });
-      expect(service.translate('key', ['string', 'expects for decimal'])).to.eql(' value ');
-    });
+test('#translate should remove the interpolation places if they are not match for the expectations', function(t) {
+  const service = createService({ key: '%s value %d' });
+  t.is(service.translate('key', ['string', 'expects for decimal']), ' value ');
+});
 
-  });
 
-  describe('#setTranslations', function() {
+test('#setTranslations should override the initial translations', function(t) {
+  const service = createService({ key: 'value' });
+  service.setTranslations({ otherKey: 'value' });
 
-    it('should override the initial translations', function() {
-      let service = createService({ key: 'value' });
-      service.setTranslations({ otherKey: 'value' });
-
-      expect(service.translate('key')).to.eql('key');
-      expect(service.translate('otherKey')).to.eql('value');
-    });
-
-  });
-
+  t.is(service.translate('key'), 'key');
+  t.is(service.translate('otherKey'), 'value');
 });
