34cef4f90f8f7f6965cde62a241c5b921653fa09
Somodi Viktor <somodi.viktor@gmail.com>
2016-02-13 12:32:38 +0100

    test(pipe): fix descriptions

--- test/pipe.spec.js
+++ test/pipe.spec.js
@@ -3,14 +3,14 @@ import 'reflect-metadata';
 import { TranslatePipe, TranslateService } from '../lib';
 import { test } from 'ava';
 
-test('#transform should delegate the translation to the service with empty arguments if there is no', function(t) {
+test('#transform should delegate the translation to the service', function(t) {
   const service = new TranslateService({ nested: { key: 'value%s' } });
   const pipe = new TranslatePipe(service);
 
-  t.is(pipe.transform('nested.key', [5]), 'value');
+  t.is(pipe.transform('nested.key', [5]), 'value5');
 });
 
-test('#transform should delegate the translation to the service', function(t) {
+test('#transform should delegate the translation to the service with empty arguments if there is no', function(t) {
   const service = new TranslateService({ nested: { key: 'value' } });
   const pipe = new TranslatePipe(service);
 
