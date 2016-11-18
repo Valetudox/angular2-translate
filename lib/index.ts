6bd91b65718129a9138d8a0341aed46cd36e0ebf
Somodi Viktor <somodi.viktor@gmail.com>
2016-11-18 07:11:17 +0100

    feat(module): export as module

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,24 @@
 export * from './service';
 export * from './pipe';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { TranslatePipe } from './pipe';
+import { TranslateService } from './service';
+
+export type Translations = {
+  [name: string]: string
+};
+
+@NgModule({
+  declarations: [TranslatePipe],
+  exports: [TranslatePipe]
+})
+export class TranslateModule {
+  static create(translations: Translations): ModuleWithProviders {
+    return {
+      ngModule: TranslateModule,
+      providers: [
+        { provide: TranslateService, useValue: new TranslateService(translations) }
+      ]
+    };
+  }
+}
